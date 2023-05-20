64c9c33c5f925cfc9ea8bdb514d9aa51fd58ccf8
MaxZRu <m@maxz.ru>
2023-05-21 02:29:33 +0500

    mac

--- ModBusServer_V5/ModBusServer_V5.ino
+++ ModBusServer_V5/ModBusServer_V5.ino
@@ -88,7 +88,7 @@ Ists
 #define MQTT_CLIENT_PASS       "XXXXXXX"
 #define MQTT_CLIENT_HOST       "XXXXXXX"
 #define MQTT_CLIENT_PORT       7498
-#define WORK_AREA               1  //1 - автодом, 2 - мира 6
+#define WORK_AREA               1  //1 - автодом, 2 -отладка (тест)
 */
 
 
